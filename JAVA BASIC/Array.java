4692490b53d23c1570af5069ba97bd2390d47f8c
Abhishek Kumar <ak204479@gmail.com>
2025-01-26 23:57:05 +0530

    Array sample

---
+++ JAVA BASIC/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[]=new int[4];
+        arr[0]=1;
+        arr[1]=5;
+        arr[2]=9;
+        arr[3]=87;
+
+        int i=0;
+        while(i<4)
+        {
+            System.out.println(arr[i]);
+            i++;
+        }
+    }
+}
