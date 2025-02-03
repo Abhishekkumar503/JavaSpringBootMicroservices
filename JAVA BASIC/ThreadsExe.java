33ea6fbcc1e737027b8e6ca2217e90d345897aaf
Abhishek Kumar <ak204479@gmail.com>
2025-02-03 23:58:41 +0530

    Threads with start() and run()

---
+++ JAVA BASIC/ThreadsExe.java
@@ -0,0 +1,25 @@
+class threadA extends Thread
+{
+    public void run()
+    {
+        for (int i = 0; i <10; i++) {
+            System.out.println("HI");
+        }
+    }
+}
+class threadB extends Thread{
+public void run()
+{
+    for (int i = 0; i <10; i++) {
+        System.out.println("HELLO");
+    }
+}
+}
+public class ThreadsExe {
+    public static void main(String[] args) {
+        threadA ta=new threadA();
+         threadB tb=new threadB();
+        ta.start();
+        tb.start();
+    }
+}
