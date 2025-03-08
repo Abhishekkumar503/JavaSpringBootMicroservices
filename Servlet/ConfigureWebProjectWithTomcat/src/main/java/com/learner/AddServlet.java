d7a2f8015e281041acf005754ddd9ac1dccbf281
Abhishek Kumar <ak204479@gmail.com>
2025-03-08 19:26:44 +0530

    send Redirect , HttpSession ,Cookies , By URL change  and Calling servlet from  servlet

--- Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/AddServlet.java
+++ Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/AddServlet.java
@@ -4,15 +4,38 @@ package com.learner;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
-	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException {
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
 		int sum = Integer.parseInt(request.getParameter("Num1")) + Integer.parseInt(request.getParameter("Num2")) ;
 		System.out.println(sum);
-		response.getWriter().println(sum);
+		response.getWriter().print("Sum is : "+ sum );
+		
+		request.setAttribute("Square", sum);
+		RequestDispatcher rd = request.getRequestDispatcher("Square");
+		rd.forward(request, response);
 	}
-
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		int sum = Integer.parseInt(request.getParameter("Num1")) - Integer.parseInt(request.getParameter("Num2")) ;
+		System.out.println(sum);
+		response.getWriter().print("Subtract is : "+ sum );
+		
+//		response.sendRedirect("Square?num=" + sum); // used for second (by sendredirect)
+		
+//		HttpSession session = request.getSession(); //By http session
+//		session.setAttribute("num", sum);
+		
+		Cookie cookie=new Cookie("num",sum+""); // by Cookies
+		response.addCookie(cookie);
+		
+		response.sendRedirect("Square");
+	}
+	
 }
---
+++ Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/SquareServlet.java
@@ -0,0 +1,51 @@
+package com.learner;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SquareServlet extends HttpServlet {
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		int num = (int)request.getAttribute("Square");
+		num *= num;
+		System.out.println(num);
+		response.getWriter().print("Square is : "+ num );
+	}
+	
+	
+	// Change by Url Method
+//	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+//		int num = Integer.parseInt(request.getParameter("num")); //num is Param which comes form AddServlet both should remain same otherwise return null exception
+//		num *= num;
+//		System.out.println(num);
+//		response.getWriter().print("Square is : "+ num );
+//	}
+	
+	
+	
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		// by providing values in session
+		
+//		HttpSession session = request.getSession();
+//		int num = (int)session.getAttribute("num");
+		
+		
+		// By Cookies
+		int num = 0;
+		Cookie[] cookies = request.getCookies();
+		for( Cookie c : cookies )
+		{
+			if(c.getName().equals("num"))
+				num = Integer.parseInt(c.getValue());
+		}
+		num *= num;
+		System.out.println(num);
+		response.getWriter().print("Square is : "+ num );
+	
+	}
+}
