7890179d59ff2aba3812f46fd016409e5cce36d1
Abhishek Kumar <ak204479@gmail.com>
2025-03-09 01:24:31 +0530

    ServletConfig and ServletContext

---
+++ Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/MyServlet.java
@@ -0,0 +1,29 @@
+package com.learner;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest rquest,HttpServletResponse response) throws IOException
+	{
+		response.getWriter().println("Hi ");
+		ServletContext ctx=getServletContext();
+		response.getWriter().println(ctx.getInitParameter("Bike"));
+	}
+
+// ctx.getInitParameter("name") --> represent as String str =ctx.getInitParameter("name")
+// response.getWriter().println("Hi "); --> printWriter out=response.getWriter(); Then out.println("Hi ");
+// 
+
+public void doPost(HttpServletRequest rquest,HttpServletResponse response) throws IOException
+{
+	response.getWriter().println("Hi ");
+	ServletConfig cg=getServletConfig();
+	response.getWriter().println(cg.getInitParameter("name"));
+}
+}
