58072b9314e13ca680fd3b60f52a05811b22a417
Abhishek Kumar <ak204479@gmail.com>
2025-04-20 19:02:24 +0530

    Fix of Filter Class

--- Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/FilterServlet.java
+++ Servlet/ConfigureWebProjectWithTomcat/src/main/java/com/learner/FilterServlet.java
@@ -3,12 +3,13 @@ package com.learner;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/FilterServlet")
 public class FilterServlet  extends HttpServlet{
 	protected void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException {
 		PrintWriter out = response.getWriter();
