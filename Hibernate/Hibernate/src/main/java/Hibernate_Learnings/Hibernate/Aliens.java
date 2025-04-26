fb30f21049902469ea846ce535c44edd98ca0e3d
Abhishek Kumar <ak204479@gmail.com>
2025-04-27 01:21:30 +0530

    Configuration hibernate

---
+++ Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/Aliens.java
@@ -0,0 +1,26 @@
+package Hibernate_Learnings.Hibernate;
+
+public class Aliens {
+
+	private int aid;
+	private String aname;
+	private String acolor;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getAcolor() {
+		return acolor;
+	}
+	public void setAcolor(String acolor) {
+		this.acolor = acolor;
+	}
+}
--- Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/App.java
+++ Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/App.java
@@ -1,5 +1,11 @@
 package Hibernate_Learnings.Hibernate;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +14,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Aliens a1 =new Aliens();
+       a1.setAid(101);
+       a1.setAname("Abhishek");
+       a1.setAcolor("fair");
+       
+       // Now for saving these details use session of Hibernate
+       
+       Configuration con = new Configuration();
+       SessionFactory sf= con.buildSessionFactory();
+       Session session = sf.openSession();
+       
     }
 }
