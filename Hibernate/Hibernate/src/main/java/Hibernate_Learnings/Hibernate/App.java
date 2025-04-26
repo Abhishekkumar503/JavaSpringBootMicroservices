2c9f5c6d344d0d9f4e8ddc68f6716e0a091c5c00
Abhishek Kumar <ak204479@gmail.com>
2025-04-26 15:09:15 +0530

    Hibernate Project Setup

---
+++ Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/App.java
@@ -0,0 +1,13 @@
+package Hibernate_Learnings.Hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
