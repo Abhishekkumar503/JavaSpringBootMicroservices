301c5c9d9835c8113d979b1b0ae3412d28936215
Abhishek Kumar <ak204479@gmail.com>
2025-05-23 00:14:29 +0530

    Hibernate Caching Level 2 with query

--- Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/Caching/CacheRunner.java
+++ Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/Caching/CacheRunner.java
@@ -7,6 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class CacheRunner {
     public static void main(String[] args) {
@@ -25,6 +26,7 @@ public class CacheRunner {
 
         Configuration con = new Configuration().configure().addAnnotatedClass(LevelCaching.class);
         SessionFactory sf = con.buildSessionFactory();
+
         Session session = sf.openSession();
         Transaction tx =session.beginTransaction();
         levelCaching = (LevelCaching) session.get(LevelCaching.class,101);
@@ -45,7 +47,23 @@ public class CacheRunner {
 
 
 
-        //Level Two Caching
+        //Level Two Caching with query
+
+        Session session2 = sf.openSession();  // this will create session
+        Transaction tx2 =session2.beginTransaction();
+
+        Query query = session2.createQuery("from LevelCaching levelcachi0_ where levelcachi0_.levelNumber=101");
+        query.setCacheable(true);
+        levelCaching = (LevelCaching) query.uniqueResult();
+        System.out.println(levelCaching);
+
+        Session session3 = sf.openSession();  // this will create session
+        Transaction tx3 =session3.beginTransaction();
+
+        Query query1 = session2.createQuery("from LevelCaching levelcachi0_ where levelcachi0_.levelNumber=101");
+        query1.setCacheable(true);
+        levelCaching = (LevelCaching) query1.uniqueResult();
+        System.out.println(levelCaching);
         
     }
 }
--- Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/Caching/LevelCaching.java
+++ Hibernate/Hibernate/src/main/java/Hibernate_Learnings/Hibernate/Caching/LevelCaching.java
@@ -1,9 +1,15 @@
 package Hibernate_Learnings.Hibernate.Caching;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
 public class LevelCaching {
     @Id
     private int levelNumber;
