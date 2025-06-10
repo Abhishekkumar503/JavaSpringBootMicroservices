84ccd60ee12d0a7c90d7923e75b9e20fe6cc8e28
Abhishek Kumar <ak204479@gmail.com>
2025-06-10 23:14:28 +0530

    tomcat jasper

---
+++ SpringMVC/src/main/java/SpringBoot/Learn/HomeController.java
@@ -0,0 +1,17 @@
+package SpringBoot.Learn;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/r")
+	public String home () {
+		return "index.jsp";
+	}
+
+}
