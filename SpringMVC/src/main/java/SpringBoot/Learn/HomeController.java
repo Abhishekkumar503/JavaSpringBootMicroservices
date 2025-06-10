d2ca7fc1ca216782e850c8f7fc8472992cf14f95
Abhishek Kumar <ak204479@gmail.com>
2025-06-11 00:04:09 +0530

    User Input param

--- SpringMVC/src/main/java/SpringBoot/Learn/HomeController.java
+++ SpringMVC/src/main/java/SpringBoot/Learn/HomeController.java
@@ -3,15 +3,31 @@ package SpringBoot.Learn;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 
 
 
 @Controller
 public class HomeController {
 
+  
 	@RequestMapping("/r")
 	public String home () {
 		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest request )
+	{
+		int firstNum = Integer.parseInt(request.getParameter("num1"));
+		int secondnum = Integer.parseInt(request.getParameter("num2"));
+		int result = firstNum + secondnum ;
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("result", result);
+		return "Result.jsp";
 	}
 
 }
--- SpringMVC/src/main/webapp/index.jsp
+++ SpringMVC/src/main/webapp/index.jsp
@@ -8,5 +8,10 @@
 </head>
 <body>
 <h1>Welcome to Spring MVC</h1>
+<form action="add">
+Enter 1st number : <input type="text" name="num1"><br>
+Enter 2nd number : <input type="text" name="num2"><br>
+<input type="Submit">
+</form>
 </body>
 </html>
