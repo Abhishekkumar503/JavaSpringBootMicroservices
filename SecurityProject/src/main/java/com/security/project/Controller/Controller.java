b7f9264974c91aa9d6eee11207d53432268c5b4c
Abhishek Kumar <ak204479@gmail.com>
2025-07-01 01:05:13 +0530

    Login

--- SecurityProject/src/main/java/com/security/project/Controller/Controller.java
+++ SecurityProject/src/main/java/com/security/project/Controller/Controller.java
@@ -14,4 +14,6 @@ public class Controller {
 	{
 		response.sendRedirect("/swagger-ui.html");
 	}
+	
+	
 }
---
+++ SecurityProject/src/main/java/com/security/project/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.security.project.SecurityConfig;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration{
+
+}
