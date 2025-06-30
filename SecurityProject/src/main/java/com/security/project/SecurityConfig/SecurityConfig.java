21e2cb55ea97c0d7360a630041a5009ecdaea518
Abhishek Kumar <ak204479@gmail.com>
2025-07-01 01:35:00 +0530

    Login Logic

--- SecurityProject/src/main/java/com/security/project/SecurityConfig/SecurityConfig.java
+++ SecurityProject/src/main/java/com/security/project/SecurityConfig/SecurityConfig.java
@@ -1,7 +1,35 @@
 package com.security.project.SecurityConfig;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-public class SecurityConfig extends WebSecurityConfiguration{
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+		
+		List<UserDetails> user =new ArrayList<>();
+		user.add(User.withDefaultPasswordEncoder().username("Abis")
+                .password("Password01")
+                .roles("USER")
+                .build());
+		return new InMemoryUserDetailsManager(user);
+	}
+        
+	
 }
