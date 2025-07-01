819f24ec70bfd2d64de6bf0f83352296b77ba7fa
Abhishek Kumar <ak204479@gmail.com>
2025-07-02 00:07:09 +0530

    Bcrypt implement

--- SecurityProject/src/main/java/com/security/project/SecurityConfig/SecurityConfig.java
+++ SecurityProject/src/main/java/com/security/project/SecurityConfig/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -44,7 +45,8 @@ public class SecurityConfig {
 	{
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());  // Simple String Password
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());			// Encrypted Format 
 		return provider;
 		
 	}
