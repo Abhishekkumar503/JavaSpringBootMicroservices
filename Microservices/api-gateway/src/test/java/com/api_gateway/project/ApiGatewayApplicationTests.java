b55f7d75472bb5cd2be4eff4c636f690ca30d5ed
ak204479@gmail.com <ak204479@gmail.com>
2025-07-09 23:34:22 +0530

    Set up API-GATEWAY

---
+++ Microservices/api-gateway/src/test/java/com/api_gateway/project/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_gateway.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
